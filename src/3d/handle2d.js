35f21df329d76c2b4093574ce44da504871fbe80
TZTMAXz <743245043@qq.com>
2022-12-02 15:27:48 +0800

    标签路由事件

--- src/3d/handle2d.js
+++ src/3d/handle2d.js
@@ -1,9 +1,45 @@
-import menu from '@/2d/hooks/useMenu'
+import { menu } from "@/2d/hooks/useMenu";
 import router from "@/2d/router";
+import store from "@/2d/store";
 
-const handleMenu = (id,name)=>{
-    if(id === 1) {
+const titleMap = {
+  '企业管理部': '企管部'
+}
 
-      // router.push();
-    }
+const getMenuBInfo = (name) => {
+  return menu.value.find(item => item.id === store.state.menuAid).children.find(item => item.name == name)
+}
+
+export const handleMenu = (id, name) => {
+  const title = titleMap[name] || name
+  if (id === 3) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
+  if (id === 7) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
+  if (id === 8) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
+  if (id === 9) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
+  if (id === 10) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
+  if (id === 11) {
+    const menuInfo = getMenuBInfo(title)
+    store.commit('setMenuBid',menuInfo.id)
+    menuInfo.path && router.push(menuInfo.path)
+  }
 }
